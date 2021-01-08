594f6fec982b6c66ca6f3f46cd0e02b2ee5195cd
Oketa Fred <oketafred@gmail.com>
2021-01-08 14:46:33 +0300

    Updated Factories and Seeds

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class CategoryFactory extends Factory
 {
@@ -21,8 +22,10 @@ class CategoryFactory extends Factory
      */
     public function definition()
     {
+        $word = $this->faker->word;
         return [
-            //
+            'name' => $word,
+            'slug' => Str::slug($word)
         ];
     }
 }
