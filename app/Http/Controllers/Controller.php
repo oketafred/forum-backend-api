2eefcdbce7497ca72e43e4e8f5c2cac330e8b92b
Oketa Fred <oketafred@gmail.com>
2021-01-19 23:00:58 +0300

    Added Documentation for Category Routes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,32 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @OA\Info(
+     *      title="Forum API Documentation", 
+     *      version="1.0.0",
+     *      description="Forum OpenApi description",
+     *      @OA\Contact( 
+     *          email="oketafred@gmail.com",
+     *      ),
+     *     @OA\License(
+     *          name="Developed by Oketa Fred",
+ *             url="https://www.linkedin.com/in/oketafred/"
+     *      )
+     * )
+     * 
+     *@OA\Server(
+     *      url="http://localhost/api",
+     *      description="Forum API Server"
+     * )
+     * 
+     * @OA\SecurityScheme(
+     *      securityScheme="apiAuth",
+     *      in="header",
+     *      bearerFormat="JWT",
+     *      type="http",
+     *      scheme="bearer"
+     * )
+     */
 }
