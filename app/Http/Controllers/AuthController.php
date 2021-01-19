826686be421d5a4eab87908a8ca4051f95b7062d
Oketa Fred <oketafred@gmail.com>
2021-01-19 23:00:36 +0300

    Added Documentation for Auth Routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -9,7 +11,37 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    /**
+     * @OA\Post(
+     * path="/auth/register",
+     * summary="Register",
+     * security={},
+     * description="Register with name, nemail, password",
+     * operationId="register",
+     * summary="Register a new user",
+     * tags={"Auth"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    @OA\JsonContent(ref="#/components/schemas/RegisterRequest")
+     * ),
+     * @OA\Response(
+     *    response="201", 
+     *    description="User Created Successfully", 
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="User Created Successfully")
+     *        )
+     *     ),
+     * ),
+     * @OA\Response(
+     *    response=422,
+     *    description="Sorry, wrong email address or password. Please try again",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Sorry, wrong email address or password. Please try again")
+     *        )
+     *     )
+     * )
+     */
+    public function register(RegisterRequest $request)
     {
         $user = User::create([
             'name' => $request->input('name'),
@@ -19,14 +51,40 @@ class AuthController extends Controller
 
         return response()->json($user, Response::HTTP_CREATED);
     }
+    
     /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Post(
+     * path="/auth/login",
+     * summary="Login a system user",
+     * security={},
+     * description="Login a user using email, password",
+     * operationId="authLogin",
+     * tags={"Auth"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    @OA\JsonContent(ref="#/components/schemas/LoginRequest")
+     * ),
+     * @OA\Response(
+     *    response="200", 
+     *    description="User loggedin successfully", 
+     *    @OA\JsonContent(),
+     * ),
+     * @OA\Response(
+     *    response=422,
+     *    description="Wrong credentials response",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Sorry, wrong email address or password. Please try again")
+     *        )
+     *     )
+     * )
      */
-    public function login()
+    public function login(LoginRequest $request)
     {
-        $credentials = request(['email', 'password']);
+        // $credentials = request(['email', 'password']);
+        $credentials = [
+                        'email'     => $request->input('email'), 
+                        'password'  => $request->input('password')
+                    ];
 
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
@@ -36,9 +94,25 @@ class AuthController extends Controller
     }
 
     /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Post(
+     * path="/me",
+     * summary="Get details of the authenticated user",
+     * description="Get details of the authenticated user",
+     * operationId="me",
+     * tags={"User"},
+     * security={{ "apiAuth": {} }},
+     * @OA\Response(
+     *    response=200,
+     *    description="Success"
+     *     ),
+     * @OA\Response(
+     *    response=401,
+     *    description="Returns when user is not authenticated",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *    )
+     * )
+     * )
      */
     public function me()
     {
@@ -46,6 +120,26 @@ class AuthController extends Controller
     }
 
     /**
+     * @OA\Post(
+     * path="/logout",
+     * summary="Logout a user",
+     * description="Logout a user and invalidate token",
+     * operationId="authLogout",
+     * tags={"Auth"},
+     * security={{ "apiAuth": {} }},
+     * @OA\Response(
+     *    response=200,
+     *    description="Success"
+     *     ),
+     * @OA\Response(
+     *    response=401,
+     *    description="Returns when user is not authenticated",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Unauthorized"),
+     *    )
+     * )
+     * )
+     *
      * Log the user out (Invalidate the token).
      *
      * @return \Illuminate\Http\JsonResponse
@@ -58,9 +152,29 @@ class AuthController extends Controller
     }
 
     /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * @OA\Post(
+     * path="/refresh",
+     * summary="Refresh a token",
+     * description="Invalidate the old token and get a new token",
+     * operationId="refresh",
+     * tags={"Auth"},
+     * security={{ "apiAuth": {} }},
+     * @OA\Response(
+     *    response=200,
+     *    description="A New Token",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="A New Token")
+     *        )
+     *     )
+     * ),
+     * @OA\Response(
+     *    response=401,
+     *    description="Returns when user is not authenticated",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *    )
+     * )
+     * )
      */
     public function refresh()
     {
