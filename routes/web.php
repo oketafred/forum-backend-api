2a9479cc7ed920d75eb30e2431cd71e5e4dbc485
Oketa Fred <oketafred@gmail.com>
2021-01-20 00:04:35 +0300

    Added Developer Profile

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Forum Backend API',
+        'developer' => 'Oketa Fred',
+        'email' => 'oketafred@gmail.com',
+        'telephone_number' => '+256 787-584-128',
+        'documentation_url' => env('L5_SWAGGER_CONST_HOST') . '/api/documentation'
+    ]);
 });
